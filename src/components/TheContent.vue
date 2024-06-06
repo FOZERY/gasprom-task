f9212454ef9a29e4be2f6d27ae81219bc6f9ec68
fozery <fozery@yandex.ru>
2024-06-06 14:40:44 +0300

    feat: finish first page styles

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -11,7 +11,57 @@
                     loading="lazy"
                 />
             </div>
-            <div class="content__form"></div>
+            <div class="content__form section">
+                <h1 class="content__header">Уважаемый клиент!</h1>
+                <p class="content__paragraph">
+                    Запрос закрыт. Пожалуйста, оцените качество предоставленного
+                    сервиса по данному обращению, используя шкалу от 0 до 9, где
+                    0 является «Хуже некуда»
+                    <span class="no-wrap">и 9 — «Отлично».</span>
+                </p>
+                <div class="content__rating rating">
+                    <ul class="content__rating-list rating__list">
+                        <li>
+                            <button
+                                class="rating__button rating__button--active"
+                            >
+                                0
+                            </button>
+                        </li>
+                        <li>
+                            <button class="rating__button">1</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">2</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">3</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">4</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">5</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">6</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">7</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">8</button>
+                        </li>
+                        <li>
+                            <button class="rating__button">9</button>
+                        </li>
+                    </ul>
+                    <div class="content__rating-caption">
+                        <span>Хуже некуда</span>
+                        <span>Отлично</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </template>
