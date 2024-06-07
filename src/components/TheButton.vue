29be5bd0d6a99fe82ca7623010905cfadb67fcca
fozery <fozery@yandex.ru>
2024-06-07 14:45:01 +0300

    feat: add button component

---
+++ src/components/TheButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    disabled: { type: Boolean, default: false },
+});
+</script>
+
+<template>
+    <button
+        type="button"
+        class="button"
+        :disabled="disabled"
+        :class="{ 'button--disabled': disabled }"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>
