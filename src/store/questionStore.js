64ba348e898f2b4fecd676c0198bb643d71a0559
fozery <fozery@yandex.ru>
2024-06-07 18:08:58 +0300

    feat: add rating and getter "filled" in questionStore

--- src/store/questionStore.js
+++ src/store/questionStore.js
@@ -3,5 +3,11 @@ import { defineStore } from 'pinia';
 export const useQuestionStore = defineStore('questionStore', {
     state: () => ({
         answers: [],
+        rating: null,
     }),
+    getters: {
+        filled: (state) => {
+            return state.answers.length === 6 && state.rating !== null;
+        },
+    },
 });
