69eec14539aeecf9ff88fe905f1a445dd0b6a829
fozery <fozery@yandex.ru>
2024-06-07 14:49:17 +0300

    feat: add ratingItemsTemplates

---
+++ src/utils/ratingItemsTemplates.js
@@ -0,0 +1,156 @@
+export const ratingItemsZeroToNine = [
+    {
+        responseId: 0,
+        round: true,
+        text: '0',
+    },
+    {
+        responseId: 1,
+        round: true,
+        text: '1',
+    },
+    {
+        responseId: 2,
+        round: true,
+        text: '2',
+    },
+    {
+        responseId: 3,
+        round: true,
+        text: '3',
+    },
+    {
+        responseId: 4,
+        round: true,
+        text: '4',
+    },
+    {
+        responseId: 5,
+        round: true,
+        text: '5',
+    },
+    {
+        responseId: 6,
+        round: true,
+        text: '6',
+    },
+    {
+        responseId: 7,
+        round: true,
+        text: '7',
+    },
+    {
+        responseId: 8,
+        round: true,
+        text: '8',
+    },
+    {
+        responseId: 9,
+        round: true,
+        text: '9',
+    },
+];
+
+export const ratingItemsOneToFive = [
+    {
+        responseId: 0,
+        round: true,
+        text: '1',
+    },
+    {
+        responseId: 1,
+        round: true,
+        text: '2',
+    },
+    {
+        responseId: 2,
+        round: true,
+        text: '3',
+    },
+    {
+        responseId: 3,
+        round: true,
+        text: '4',
+    },
+    {
+        responseId: 4,
+        round: true,
+        text: '5',
+    },
+];
+
+export const ratingItemsZeroToTen = [
+    {
+        responseId: 0,
+        round: true,
+        text: '0',
+    },
+    {
+        responseId: 1,
+        round: true,
+        text: '1',
+    },
+    {
+        responseId: 2,
+        round: true,
+        text: '2',
+    },
+    {
+        responseId: 3,
+        round: true,
+        text: '3',
+    },
+    {
+        responseId: 4,
+        round: true,
+        text: '4',
+    },
+    {
+        responseId: 5,
+        round: true,
+        text: '5',
+    },
+    {
+        responseId: 6,
+        round: true,
+        text: '6',
+    },
+    {
+        responseId: 7,
+        round: true,
+        text: '7',
+    },
+    {
+        responseId: 8,
+        round: true,
+        text: '8',
+    },
+    {
+        responseId: 9,
+        round: true,
+        text: '9',
+    },
+    {
+        responseId: 10,
+        round: true,
+        text: '10',
+    },
+];
+
+export const ratingItemsTimeQuestion = [
+    {
+        responseId: 0,
+        round: false,
+        text: 'Быстрее, чем ожидал',
+    },
+    {
+        responseId: 1,
+        round: false,
+        text: 'Как и ожидал',
+    },
+    {
+        responseId: 2,
+        round: false,
+        text: 'Медленнее, чем ожидал',
+    },
+];
