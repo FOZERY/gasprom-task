2dd3adc645ed2e739b447b4cf85fdf68b7a6ae61
fozery <fozery@yandex.ru>
2024-06-07 14:45:13 +0300

    feat: add button-rating component

---
+++ src/components/ButtonRating.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    questionId: { type: Number, default: null },
+    responseId: { type: Number, default: null },
+    activeId: { type: Number, default: null },
+    round: { type: Boolean, default: false },
+});
+</script>
+
+<template>
+    <button
+        type="button"
+        class="button rating__button"
+        :class="{
+            'rating__button--active': activeId === responseId,
+            'rating__button--round': round,
+        }"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>
