5b9f6a6796870510d7fb77fdbcd8e66e2580c240
fozery <fozery@yandex.ru>
2024-06-07 14:45:45 +0300

    feat: add third and fourth pages

---
+++ src/pages/PageFourth.vue
@@ -0,0 +1,36 @@
+<script setup>
+import TheContent from '@/components/TheContent.vue';
+import TheContentImage from '@/components/TheContentImage.vue';
+import ButtonRating from '@/components/ButtonRating.vue';
+import TheContentForm from '@/components/TheContentForm.vue';
+
+import { ref } from 'vue';
+
+const activeId = ref(null);
+</script>
+
+<template>
+    <main class="content">
+        <div class="content__inner container">
+            <TheContentImage
+                :isSmaller="true"
+                :image="'/images/background_image_fourth.png'"
+            />
+            <TheContentForm>
+                <template #header>Вы уже прошли этот опрос</template>
+                <template #body>
+                    <p class="content__paragraph paragraph">
+                        Спасибо, что делитесь мнением
+                        <br />
+                        и помогаете нам быть лучше
+                    </p>
+                    <a href="https://rutube.ru/" class="content__button button"
+                        >Перейти на RUTUBE</a
+                    >
+                </template>
+            </TheContentForm>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
---
+++ src/pages/PageThird.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheContentImage from '@/components/TheContentImage.vue';
+import TheContentForm from '@/components/TheContentForm.vue';
+</script>
+
+<template>
+    <main class="content">
+        <div class="content__inner container">
+            <TheContentImage :image="'/images/background_image_third.png'" />
+            <TheContentForm>
+                <template #header>Спасибо за обратную связь!</template>
+                <template #body>
+                    <p class="content__paragraph paragraph">
+                        Это поможет нам улучшить сервис
+                    </p>
+                    <a href="https://rutube.ru/" class="content__button button"
+                        >Перейти на платформу</a
+                    >
+                </template>
+            </TheContentForm>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
